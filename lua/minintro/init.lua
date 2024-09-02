4944fb2d22f72daf9e327ce0bb23b83e20ad126d
Owen L <32573897+mageowl@users.noreply.github.com>
2024-09-02 11:14:10 -0700

    add customizable logo

--- lua/minintro/init.lua
+++ lua/minintro/init.lua
@@ -1,18 +1,21 @@
 -- local minintro_opened = false
 
-local intro_logo = {
+local DEFAULT_LOGO = {
 	" ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗",
 	" ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║",
 	" ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║",
 	" ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║",
 	" ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║",
-	" ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝"
+	" ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝",
 }
 
 local PLUGIN_NAME = "minintro"
-local DEFAULT_COLOR = "#98c379"
-local INTRO_LOGO_HEIGHT = #intro_logo
-local INTRO_LOGO_WIDTH = 55
+local DEFAULT_OPTIONS = {
+	color = "#98c379",
+	logo = DEFAULT_LOGO,
+	logo_width = 55,
+}
+local intro_logo, intro_logo_width, intro_logo_height
 
 local autocmd_group = vim.api.nvim_create_augroup(PLUGIN_NAME, {})
 local highlight_ns_id = vim.api.nvim_create_namespace(PLUGIN_NAME)
@@ -33,14 +36,20 @@ local function draw_minintro(buf, logo_width, logo_height)
 
 	local start_col = math.floor((screen_width - logo_width) / 2)
 	local start_row = math.floor((screen_height - logo_height) / 2)
-	if (start_col < 0 or start_row < 0) then return end
+	if start_col < 0 or start_row < 0 then
+		return
+	end
 
 	local top_space = {}
-	for _ = 1, start_row do table.insert(top_space, "") end
+	for _ = 1, start_row do
+		table.insert(top_space, "")
+	end
 
 	local col_offset_spaces = {}
-	for _ = 1, start_col do table.insert(col_offset_spaces, " ") end
-	local col_offset = table.concat(col_offset_spaces, '')
+	for _ = 1, start_col do
+		table.insert(col_offset_spaces, " ")
+	end
+	local col_offset = table.concat(col_offset_spaces, "")
 
 	local adjusted_logo = {}
 	for _, line in ipairs(intro_logo) do
@@ -53,8 +62,8 @@ local function draw_minintro(buf, logo_width, logo_height)
 	lock_buf(buf)
 
 	vim.api.nvim_buf_set_extmark(buf, highlight_ns_id, start_row, start_col, {
-		end_row = start_row + INTRO_LOGO_HEIGHT,
-		hl_group = "Default"
+		end_row = start_row + intro_logo_height,
+		hl_group = "Default",
 	})
 end
 
@@ -73,18 +82,18 @@ local function create_and_set_minintro_buf(default_buff)
 end
 
 local function set_options()
-	vim.opt_local.number = false         -- disable line numbers
+	vim.opt_local.number = false -- disable line numbers
 	vim.opt_local.relativenumber = false -- disable relative line numbers
-	vim.opt_local.list = false           -- disable displaying whitespace
-	vim.opt_local.fillchars = { eob = ' ' } -- do not display "~" on each new line
-	vim.opt_local.colorcolumn = "0"      -- disable colorcolumn
+	vim.opt_local.list = false -- disable displaying whitespace
+	vim.opt_local.fillchars = { eob = " " } -- do not display "~" on each new line
+	vim.opt_local.colorcolumn = "0" -- disable colorcolumn
 end
 
 local function redraw()
 	unlock_buf(minintro_buff)
 	vim.api.nvim_buf_set_lines(minintro_buff, 0, -1, true, {})
 	lock_buf(minintro_buff)
-	draw_minintro(minintro_buff, INTRO_LOGO_WIDTH, INTRO_LOGO_HEIGHT)
+	draw_minintro(minintro_buff, intro_logo_width, intro_logo_height)
 end
 
 local function display_minintro(payload)
@@ -100,27 +109,33 @@ local function display_minintro(payload)
 	minintro_buff = create_and_set_minintro_buf(default_buff)
 	set_options()
 
-	draw_minintro(minintro_buff, INTRO_LOGO_WIDTH, INTRO_LOGO_HEIGHT)
+	draw_minintro(minintro_buff, intro_logo_width, intro_logo_height)
 
 	vim.api.nvim_create_autocmd({ "WinResized", "VimResized" }, {
 		group = autocmd_group,
 		buffer = minintro_buff,
-		callback = redraw
+		callback = redraw,
 	})
 end
 
 local function setup(options)
-	options = options or {}
-	vim.api.nvim_set_hl(highlight_ns_id, "Default", { fg = options.color or DEFAULT_COLOR })
+	options = vim.tbl_extend("force", DEFAULT_OPTIONS, options)
+
+	vim.api.nvim_set_hl(highlight_ns_id, "Default", { fg = options.color })
 	vim.api.nvim_set_hl_ns(highlight_ns_id)
+
+	intro_logo = options.logo
+	intro_logo_width = options.logo_width
+	intro_logo_height = #options.logo
+	print(intro_logo_width, intro_logo_height)
 
 	vim.api.nvim_create_autocmd("VimEnter", {
 		group = autocmd_group,
 		callback = display_minintro,
-		once = true
+		once = true,
 	})
 end
 
 return {
-	setup = setup
+	setup = setup,
 }
